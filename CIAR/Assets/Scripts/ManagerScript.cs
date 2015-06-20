9f28a9c82c4cb1a5e38d8f9b194fef1b9dbaac5b
Kayron Cabral <kayron7@gmail.com>
2015-06-20 12:08:21 -0400

    atualizando scritp e adicionado cabelos a cena

--- CIAR/Assets/Scripts/ManagerScript.cs
+++ CIAR/Assets/Scripts/ManagerScript.cs
@@ -1,23 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Vuforia;
 
 public class ManagerScript : MonoBehaviour {
-
-
-
-	// Use this for initialization
-	void Start () {
 	
+	public GameObject cameraButton;
+	public GameObject switchCameremaButton;
+	private bool takeScreenshot;
+	private CameraDevice.CameraDirection direction = CameraDevice.CameraDirection.CAMERA_BACK;
+
+	public void Start () {
+
+		takeScreenshot = false;
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public void Update () {
 
+	}
+	
+	public void LateUpdate () {
+		
+		if (takeScreenshot) {
+			takePicture();
+		}
+		
+	}
+	
+	public void setTakeScreenshot (bool status) {
+		takeScreenshot = status;
+	}
+	
 	public void takePicture () {
-
-		Debug.Log("Taking Picture");
-
+		Debug.Log ("Taking Picture");
+		cameraButton.SetActive (false);
+		switchCameremaButton.SetActive (false);
+		Application.CaptureScreenshot ("Screenshot.jpeg", 4);
+		cameraButton.SetActive (true);
+		switchCameremaButton.SetActive (true);
+		setTakeScreenshot (false);
+		CameraDevice.Instance.Stop ();
+		CameraDevice.Instance.Start ();
+		//		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+		
+	}
+	
+	public void switchCamera () {
+		if (direction == CameraDevice.CameraDirection.CAMERA_BACK) {
+			CameraDevice.Instance.Stop ();
+			CameraDevice.Instance.Init (CameraDevice.CameraDirection.CAMERA_FRONT);
+			CameraDevice.Instance.Start ();
+			direction = CameraDevice.CameraDirection.CAMERA_FRONT;
+		} else {
+			CameraDevice.Instance.Stop ();
+			CameraDevice.Instance.Init (CameraDevice.CameraDirection.CAMERA_BACK);
+			CameraDevice.Instance.Start ();
+			direction = CameraDevice.CameraDirection.CAMERA_BACK;
+		}
 	}
 }
