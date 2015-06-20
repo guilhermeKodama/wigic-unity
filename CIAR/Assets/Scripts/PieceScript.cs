75169e9aaf1ee33b9c738736c4eb93348bf8207c
Kayron Cabral <kayron7@gmail.com>
2015-06-20 12:23:45 -0400

    Criando script da peça do quebra cabeça

---
+++ CIAR/Assets/Scripts/PieceScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceScript : MonoBehaviour {
+	
+	public float raySize = 300f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Debug.DrawRay (transform.position, Vector3.right * raySize);
+
+	}
+}
