cf1faba646aba25924dccdd505c7e4067f75dfd8
Kayron Cabral <kayron7@gmail.com>
2015-06-20 21:55:40 -0400

    criando cubos de teste de colisão

--- CIAR/Assets/Scripts/PieceScript.cs
+++ CIAR/Assets/Scripts/PieceScript.cs
@@ -13,7 +13,31 @@ public class PieceScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		Debug.DrawRay (transform.position, Vector3.right * raySize);
+		RaycastHit hit;
+
+		Debug.DrawRay (transform.position, Vector3.right * raySize, Color.red);
+		Debug.DrawRay (transform.position, Vector3.left * raySize, Color.red);
+		Debug.DrawRay (transform.position, Vector3.back * raySize, Color.red);
+		Debug.DrawRay (transform.position, Vector3.forward * raySize, Color.red);
+
+		if (Physics.Raycast (new Ray (transform.position, Vector3.left), out hit, raySize) || 
+			Physics.Raycast (new Ray (transform.position, Vector3.right), out hit, raySize) || 
+			Physics.Raycast (new Ray (transform.position, Vector3.back), out hit, raySize) || 
+			Physics.Raycast (new Ray (transform.position, Vector3.forward), out hit, raySize)) {
+
+			Debug.Log ("Contato!");
+
+			if (hit.collider.tag == "Piece") {
+
+				Debug.Log ("Piece!");
+
+				gameObject.GetComponent<MeshRenderer> ().material.color = Color.green;
+			}
+		} else {
+		
+			gameObject.GetComponent<MeshRenderer> ().material.color = Color.gray;
+
+		}
 
 	}
 }
