4df1cb286c11097ed93ebf59b34e9fdf7fb754f5
Kayron Cabral <kayron7@gmail.com>
2015-06-29 13:04:42 -0400

    criando script de mudança de cabelos e ajustando à cena

---
+++ CIAR/Assets/Scripts/WigManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WigManagerScript : MonoBehaviour {
+
+	public GameObject[] wigsObj;
+	public GameObject currtenWig;
+
+	private GameObject wig;
+	private ArrayList wigs;
+	private int index;
+	private bool slid;
+
+	void Start () {
+		index = 0;
+		wigs = new ArrayList (wigsObj);
+		wig = wigs [index] as GameObject;
+		currtenWig.gameObject.GetComponent<SpriteRenderer>().sprite =  wig.gameObject.GetComponent<SpriteRenderer>().sprite;
+	}
+
+	void Update () {
+
+	}
+
+	void OnSwipeLeft () {
+		if (index == 0) {
+			index = wigs.Count - 1;
+		} else {
+			index--;
+		}
+		wig = wigs [index] as GameObject;
+		currtenWig.gameObject.GetComponent<SpriteRenderer> ().sprite = wig.gameObject.GetComponent<SpriteRenderer> ().sprite;
+	}
+
+	void OnSwipeRight () {
+		if (index == wigs.Count - 1) {
+			index = 0;
+		} else {
+			index++;
+		}
+		wig = wigs [index] as GameObject;
+		currtenWig.gameObject.GetComponent<SpriteRenderer> ().sprite = wig.gameObject.GetComponent<SpriteRenderer> ().sprite;
+	}
+
+}
